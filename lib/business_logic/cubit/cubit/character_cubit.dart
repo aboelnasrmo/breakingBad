752e8d51febdcfff867c7740cac09d5bf0539ecb
mohammadaboelnasr <m.aboelnasr@live.com>
2022-11-25 18:03:29 +0300

    backend done

--- lib/business_logic/cubit/cubit/character_cubit.dart
+++ lib/business_logic/cubit/cubit/character_cubit.dart
@@ -1,8 +1,20 @@
+import 'package:approutertrial/data/model/character_model.dart';
+import 'package:approutertrial/data/repository/character_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'character_state.dart';
 
 class CharacterCubit extends Cubit<CharacterState> {
-  CharacterCubit() : super(CharacterInitial());
+  final CharactersRepository charactersRepository;
+  late List<Character> characters;
+  CharacterCubit(this.charactersRepository) : super(CharacterInitial());
+
+  List<Character> getAllCharacters() {
+    charactersRepository.getAllCharacters().then((characters) {
+      emit(CharacterIsloaded(characters));
+      this.characters = characters;
+    });
+    return characters;
+  }
 }
--- lib/business_logic/cubit/cubit/character_state.dart
+++ lib/business_logic/cubit/cubit/character_state.dart
@@ -4,3 +4,9 @@ part of 'character_cubit.dart';
 abstract class CharacterState {}
 
 class CharacterInitial extends CharacterState {}
+
+class CharacterIsloaded extends CharacterState {
+  //this what will i get if it's loaded succesfully
+  final List<Character> characters;
+  CharacterIsloaded(this.characters);
+}
