90701e00dd652a235e136744f7150562bcbf3572
mohammadaboelnasr <m.aboelnasr@live.com>
2024-04-01 21:30:10 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@ import 'package:approutertrial/helper/router/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp(
-    appRouter: AppRouter(),
-  ));
+  runApp(
+    MyApp(
+      appRouter: AppRouter(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
