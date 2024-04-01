339e8a71e9364cce4c1758a76b601519d11d80bb
mohammadaboelnasr <m.aboelnasr@live.com>
2024-04-01 23:27:14 +0300

    Remove commented code in CharacterIsloaded class

--- lib/business_logic/cubit/cubit/character_state.dart
+++ lib/business_logic/cubit/cubit/character_state.dart
@@ -6,7 +6,6 @@ abstract class CharacterState {}
 class CharacterInitial extends CharacterState {}
 
 class CharacterIsloaded extends CharacterState {
-  //this what will i get if it's loaded succesfully
   final List<Character> characters;
   CharacterIsloaded(this.characters);
 }
